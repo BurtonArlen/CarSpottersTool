e66c439b7e53d86b8eb00f26422dfccca47140cf
Guest User <Guest@Epicodus-5D.local>
2017-06-19 08:19:46 -0700

    added empty activities for mvp

---
+++ app/src/main/java/com/example/guest/carspotterstool/Constants.java
@@ -0,0 +1,8 @@
+package com.example.guest.carspotterstool;
+
+/**
+ * Created by Guest on 6/19/17.
+ */
+
+public class Constants {
+}
--- app/src/main/java/com/example/guest/carspotterstool/ui/SearchActivity.java
+++ app/src/main/java/com/example/guest/carspotterstool/ui/SearchActivity.java
@@ -1,13 +1,15 @@
-package com.example.guest.carspotterstool;
+package com.example.guest.carspotterstool.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignIn extends AppCompatActivity {
+import com.example.guest.carspotterstool.R;
+
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_search);
     }
 }
